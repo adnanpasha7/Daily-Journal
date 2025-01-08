5a81a564e1cb1033938fbc49c064c677debc2760
Adnan Pasha <92546855+adnanpasha7@users.noreply.github.com>
2025-01-08 17:30:20 +0000

    Implementation

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+import Faqs from "@/sections/Faqs";
+import Features from "@/sections/Features";
+import Hero from "@/sections/Hero";
+import Integrations from "@/sections/Integrations";
+import Introduction from "@/sections/Introduction";
+import LogoTicker from "@/sections/LogoTicker";
+import Navbar from "@/sections/Navbar";
+import CallToAction from "../sections/CallToAction";
+import Footer from "@/sections/Footer";
+
+export default function Home() {
+    return <>
+        <Navbar />
+        <Hero />
+        <LogoTicker />
+        <Introduction />
+        <Features />
+        <Integrations />
+        <Faqs />
+        <CallToAction />
+        <Footer />
+    </>;
+}
---
+++ src/sections/Footer.tsx
@@ -0,0 +1,36 @@
+import logoImage from "@/assets/images/logo.svg";
+import Image from "next/image";
+import Link from "next/link";
+
+const footerLinks = [
+    { href: "#", label: "Contact" },
+    { href: "#", label: "Privacy Policy" },
+    { href: "#", label: "Terms & Conditions" },
+];
+
+export default function Footer() {
+    return (
+        <section className="py-16">
+            <div className="container">
+                <div className="flex flex-col md:flex-row items-center md:justify-between gap-6">
+                    <div>
+                        <Image src={logoImage} alt="Layer logo" />
+                    </div>
+                    <div>
+                        <nav className="flex gap-6">
+                            {footerLinks.map((link) => (
+                                <Link
+                                    href={link.href}
+                                    key={link.label}
+                                    className="text-white/50 text-sm"
+                                >
+                                    {link.label}
+                                </Link>
+                            ))}
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
